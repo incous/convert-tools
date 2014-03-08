5d769ac6267fdd8dbf8ae89529139c2d28f7c4e3
incous <incous@users.noreply.github.com>
2014-03-08 12:23:27 +0700

    change print htmlContent => fwrite

--- LanGuard-xml2html.py
+++ LanGuard-xml2html.py
@@ -3,58 +3,59 @@
 import sys
 from xml.dom.minidom import parse
 
+f = open(sys.argv[2], 'w')
 xmlContent = parse(sys.argv[1])
 hostList = xmlContent.getElementsByTagName('host')
-htmlContent = '''<html>
+f.write('''<html>
 <head>
 <title>Host assessment report</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <body>
-'''
+''')
 
 for host in hostList:
 	# extract host information
 	hostname = 'N/A' if len(host.getElementsByTagName('hostname')[0].childNodes) == 0 else host.getElementsByTagName('hostname')[0].childNodes[0].data
-	htmlContent += '''<h1>Host %s</h1>
+	f.write('''<h1>Host %s</h1>
 <h2>System information</h2>
 <ul>
 <li>Hostname: %s</li>
-''' % (hostname, hostname)
+''' % (hostname, hostname))
 	ip = host.getElementsByTagName('ip')[0].childNodes[0].data
-	htmlContent += '''<li>IP address: %s</li>
-''' % ip
+	f.write('''<li>IP address: %s</li>
+''' % ip)
 	os = '' if len(host.getElementsByTagName('os')[0].childNodes) == 0 else host.getElementsByTagName('os')[0].childNodes[0].data
 	servicesPack = '' if len(host.getElementsByTagName('servpack')[0].childNodes) == 0 else host.getElementsByTagName('servpack')[0].childNodes[0].data
 	os = os + ' SP' + servicesPack if servicesPack != '' else os
-	htmlContent += '''<li>OS: %s</li>
-</ul>''' % os
+	f.write('''<li>OS: %s</li>
+</ul>''' % os)
 	if len(host.getElementsByTagName('apps_installed')) == 0: continue
-	htmlContent += '''<h2>List of installed applications</h2>
+	f.write('''<h2>List of installed applications</h2>
 <table border="1">
 <tr>
 	<td>Name</td><td>Version</td><td>Publisher</td><td>Up-to-date</td>
 </tr>
-'''
+''')
 	installedApps = host.getElementsByTagName('apps_installed')[0]
 	for app in installedApps.childNodes:
 		appName = app.getAttribute('name')
 		appVersion = app.getAttribute('version')
 		appVendor = app.getAttribute('publisher')
 		appUp2date = app.getAttribute('is_up_to_date')
-		htmlContent += '''<tr>
+		f.write('''<tr>
 	<td>%s</td><td>%s</td><td>%s</td><td>%s</td>
 </tr>
-''' % (appName, appVersion, appVendor, appUp2date)
-	htmlContent += '''</table>
-'''
+''' % (appName, appVersion, appVendor, appUp2date))
+	f.write('''</table>
+''')
 	if len(host.getElementsByTagName('alerts')) == 0: continue
-	htmlContent += '''<h2>List of vulnerabilities</h2>
+	f.write('''<h2>List of vulnerabilities</h2>
 <table border="1">
 <tr>
 	<td>Title</td><td>Description</td><td>Category</td><td>Severity</td>
 </tr>
-'''
+''')
 	vulList = host.getElementsByTagName('alerts')[0].getElementsByTagName('alert')
 	for vul in vulList:
 		vulName = vul.getElementsByTagName('name')[0].childNodes[0].data
@@ -64,29 +65,29 @@ for host in hostList:
 			break
 		else:
 			vulSeverity = vul.parentNode.parentNode.getAttribute('level')
-		htmlContent += '''<tr>
+		f.write('''<tr>
 	<td>%s</td><td>%s</td><td>%s</td><td>%s</td>
 </tr>
-''' % (vulName, vulDescription, vulCategory, vulSeverity)
-	htmlContent += '''</table>
-'''
+''' % (vulName, vulDescription, vulCategory, vulSeverity))
+	f.write('''</table>
+''')
 	# if len(host.getElementsByTagName('alerts')[0].getElementsByTagName('missing_hotfixes')) == 0: continue
-	htmlContent += '''<h2>List of available hotfixes</h2>
+	f.write('''<h2>List of available hotfixes</h2>
 <table border="1">
 <tr>
 	<td>Bulletin ID</td><td>Name</td><td>Release date</td><td>Severity</td>
 </tr>
-'''
+''')
 	hotfixes = host.getElementsByTagName('alerts')[0].getElementsByTagName('missing_hotfixes')[0].getElementsByTagName('hotfix')
 	for hotfix in hotfixes:
 		hotfixBID = hotfix.getElementsByTagName('bulletinid')[0].childNodes[0].data
 		hotfixTitle = hotfix.getElementsByTagName('title')[0].childNodes[0].data
 		hotfixDate = hotfix.getElementsByTagName('date')[0].childNodes[0].data
 		hotfixSeverity = 'N/A' if len(hotfix.getElementsByTagName('severity')[0].childNodes) == 0 else hotfix.getElementsByTagName('severity')[0].childNodes[0].data
-		htmlContent += '''<tr>
+		f.write('''<tr>
 	<td>%s</td><td>%s</td><td>%s</td><td>%s</td>
 </tr>
-''' % (hotfixBID, hotfixTitle, hotfixDate, hotfixSeverity)
-	htmlContent += '</table><hr />'
-htmlContent += '</body></html>'
-print htmlContent.encode('utf-8')
+''' % (hotfixBID, hotfixTitle, hotfixDate, hotfixSeverity))
+	f.write('</table><hr />')
+f.write('</body></html>')
+f.close()
