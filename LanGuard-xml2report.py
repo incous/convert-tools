7b5effa5cbde740af5fded56da2c7b3befd3e82c
Thang Kieu <incous@yahoo.com>
2014-03-12 12:27:24 +0700

    Localization

--- LanGuard-xml2report.py
+++ LanGuard-xml2report.py
@@ -16,8 +16,8 @@ f.write('''<html>
 
 for host in hostList:
 	hostname = 'N/A' if len(host.getElementsByTagName('hostname')[0].childNodes) == 0 else host.getElementsByTagName('hostname')[0].childNodes[0].data
-	f.write(('''<h1>Host %s</h1>
-''' % (hostname)).encode('utf-8'))
+	f.write(('''<h1>Máy %s</h1>
+'''.decode('utf-8') % (hostname)).encode('utf-8'))
 	
 # 	if len(host.getElementsByTagName('apps_installed')) == 0: continue
 # 	f.write('''<h2>List of installed applications</h2>
@@ -39,32 +39,47 @@ for host in hostList:
 # 	f.write('''</table>
 # ''')
 	if len(host.getElementsByTagName('alerts')) == 0: continue
-	f.write('''<h2>List of vulnerabilities</h2>
+	f.write('''<h2>Danh sách lỗi được phân loại theo mức độ nguy hiểm</h2>
 <table border="1">
 <tr>
-	<td>Title</td><td>Description</td><td>Category</td><td>Severity</td>
+	<td>Danh sách lỗi</td><td>Phân loại lỗi</td><td>Mức độ nguy hiểm</td>
 </tr>
 ''')
 	vulList = host.getElementsByTagName('alerts')[0].getElementsByTagName('alert')
 	for vul in vulList:
 		vulName = vul.getElementsByTagName('name')[0].childNodes[0].data
-		vulDescription = '' if len(vul.getElementsByTagName('descr')[0].childNodes) ==0 else vul.getElementsByTagName('descr')[0].childNodes[0].data
+		# vulDescription = '' if len(vul.getElementsByTagName('descr')[0].childNodes) ==0 else vul.getElementsByTagName('descr')[0].childNodes[0].data
 		vulCategory = vul.parentNode.nodeName
-		if vulCategory == 'Information_Alerts':
-			break
+		if vulCategory == 'Software_Alerts':
+			vulCategoryLabel = 'Phần mềm'
+		elif vulCategory == 'Registry_Alerts':
+			vulCategoryLabel = 'Cấu hình hệ thống'
+		elif vulCategory == 'Services_Alerts':
+			vulCategoryLabel = 'Dịch vụ ứng dụng khác'
+		elif vulCategory == 'Information_Alerts':
+			continue
 		else:
-			vulSeverity = vul.parentNode.parentNode.getAttribute('level')
+			vulCategoryLabel = 'N/A'
+		vulSeverity = vul.parentNode.parentNode.getAttribute('level')
+		if vulSeverity == ' 0':
+			vulSeverityLabel = 'Nghiêm trọng'
+		elif vulSeverity == ' 1':
+			vulSeverityLabel = 'Trung bình'
+		elif vulSeverity == ' 2':
+			vulSeverityLabel = 'Thấp'
+		else:
+			vulSeverityLabel = 'N/A'
 		f.write(('''<tr>
-	<td>%s</td><td>%s</td><td>%s</td><td>%s</td>
+	<td>%s</td><td>%s</td><td>%s</td>
 </tr>
-''' % (vulName, vulDescription, vulCategory, vulSeverity)).encode('utf-8'))
+''' % (vulName, vulCategoryLabel.decode('utf-8'), vulSeverityLabel.decode('utf-8'))).encode('utf-8'))
 	f.write('''</table>
 ''')
 	if len(host.getElementsByTagName('hotfixes')[0].getElementsByTagName('hotfix')) == 0: continue
-	f.write('''<h2>List of available hotfixes</h2>
+	f.write('''<h2>Thông tin bản vá còn thiếu</h2>
 <table border="1">
 <tr>
-	<td>Bulletin ID</td><td>Name</td><td>Release date</td><td>Severity</td>
+	<td>Danh sách bản vá</td><td>Ngày phát hành</td><td>Mức độ nguy hiểm</td>
 </tr>
 ''')
 	hotfixes = host.getElementsByTagName('hotfixes')[0].getElementsByTagName('hotfix')
@@ -73,10 +88,18 @@ for host in hostList:
 		hotfixTitle = hotfix.getElementsByTagName('title')[0].childNodes[0].data
 		hotfixDate = hotfix.getElementsByTagName('date')[0].childNodes[0].data
 		hotfixSeverity = 'N/A' if len(hotfix.getElementsByTagName('severity')[0].childNodes) == 0 else hotfix.getElementsByTagName('severity')[0].childNodes[0].data
+		if hotfixSeverity == 'Critical' or hotfixSeverity == 'Important':
+			hotfixSeverityLabel = 'Nghiêm trọng'
+		elif hotfixSeverity == 'Moderate':
+			hotfixSeverityLabel = 'Trung bình'
+		elif hotfixSeverity == 'Low' or hotfixSeverity == 'N/A':
+			hotfixSeverityLabel = 'Thấp'
+		else:
+			hotfixSeverityLabel = 'N/A'
 		f.write(('''<tr>
-	<td>%s</td><td>%s</td><td>%s</td><td>%s</td>
+	<td>%s</td><td>%s</td><td>%s</td>
 </tr>
-''' % (hotfixBID, hotfixTitle, hotfixDate, hotfixSeverity)).encode('utf-8'))
+''' % (hotfixBID, hotfixDate, hotfixSeverityLabel.decode('utf-8'))).encode('utf-8'))
 	f.write('</table><hr />')
 f.write('</body></html>')
 f.close()
